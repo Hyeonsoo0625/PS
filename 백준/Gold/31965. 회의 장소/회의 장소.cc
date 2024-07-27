cd3aee3581f2c88401d782672d4c1d8e3d8c80d4
Hyeonsoo Kim <102412616+hyeonsoo0625@users.noreply.github.com>
2024-07-27 19:51:57 +0900

    [Gold II] Title: 회의 장소, Time: 176 ms, Memory: 5144 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31965. 회의 장소/회의 장소.cc
+++ 백준/Gold/31965. 회의 장소/회의 장소.cc
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int N, Q, a, L, R;
-vector<long long> v, sum;
-#include <algorithm>
-int binary_search(int target, vector<int> v, int a) {
+long long  v[200010], sum[200010];
+
+int binary_search(int target, long long v[], int a, int vsize) {
 	int left = 0;
-	int right = v.size() - 1;
+	int right = vsize - 1;
 	int mid = 0;
 	while (left <= right) {
 		mid = (left + right) / 2;
@@ -28,27 +27,25 @@ int binary_search(int target, vector<int> v, int a) {
 		return right;
 	}
 }
-long long getDis(int low, int high, int j) {
-	long long left = (long long)(j - low + 1) * v[j] - (sum[j] - sum[low - 1]);
-	long long right = (sum[high] - sum[j - 1]) - (long long)(high - j + 1) * v[j];
 
-	return left + right;
+long long getDis(int low, int high, int j) {
+	return (long long)(2 * j - low - high) * v[j] - (sum[j] - sum[low - 1]) + (sum[high] - sum[j - 1]);
 }
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
 	cin >> N >> Q;
-	sum.emplace_back(0);
-	v.emplace_back(0);
 	for (int i = 1; i <= N; i++) {
 		cin >> a;
-		v.emplace_back(a);
-		sum.emplace_back(sum[i - 1] + a);
+		v[i] = a;
+		sum[i] = sum[i - 1] + a;
 	}
 	for (int i = 0; i < Q; i++) {
 		cin >> L >> R;
-//		int lidx = binary_search(L, v, 0);
-//		int ridx = binary_search(R, v, 1);
-		int lidx = lower_bound(v.begin(), v.end(), L) - v.begin();
-		int ridx = upper_bound(v.begin(), v.end(), R) - v.begin() - 1;
+		int lidx = binary_search(L, v, 0, N+1);
+		int ridx = binary_search(R, v, 1, N+1);
 		if (lidx >= ridx) {
 			cout << 0 << '\n';
 			continue;
